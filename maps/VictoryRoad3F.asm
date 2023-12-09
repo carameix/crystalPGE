8a8304b778989b334189ec398d9a34482149218e
carameix <58843379+carameix@users.noreply.github.com>
2023-12-09 17:30:37 +0000

    Added Victory Road boulder down pit handling
    Used Ice Path as a template for boulder-down-pit handling using StoneTable. Learnt that the on map asm file, the order of objects in object_const_def seems to correspond to the order of def_object_events at the bottom of the file.

--- maps/VictoryRoad3F.asm
+++ maps/VictoryRoad3F.asm
@@ -1,4 +1,8 @@
 	object_const_def
+	const VICTORYROAD3F_BOULDER1
+	const VICTORYROAD3F_BOULDER2
+	const VICTORYROAD3F_BOULDER3
+	const VICTORYROAD3F_BOULDER4
 	;const VICTORYROAD_RIVAL
 	;const VICTORYROAD_POKE_BALL1
 	;const VICTORYROAD_POKE_BALL2
@@ -10,9 +14,45 @@ VictoryRoad3F_MapScripts:
 	def_scene_scripts
 	
 	def_callbacks
+	callback MAPCALLBACK_CMDQUEUE, VictoryRoad3FSetUpStoneTableCallback
+	
+VictoryRoad3FSetUpStoneTableCallback:
+	writecmdqueue .CommandQueue
+	endcallback
+	
+.CommandQueue:
+	cmdqueue CMDQUEUE_STONETABLE, .StoneTable ; check if any stones are sitting on a warp
+
+.StoneTable:
+	stonetable 5, VICTORYROAD3F_BOULDER1, .Boulder1
+	db -1 ; end
+	
+.Boulder1:
+	disappear VICTORYROAD3F_BOULDER1
+	clearevent EVENT_BOULDER_IN_VICTORY_ROAD_1A
+	sjump .FinishBoulder
+	
+.FinishBoulder:
+	pause 30
+	scall .BoulderFallsThrough
+	opentext
+	writetext VictoryRoadBoulderFellThroughText
+	waitbutton
+	closetext
+	end
+
+.BoulderFallsThrough:
+	playsound SFX_STRENGTH
+	earthquake 80
+	end
 
 VictoryRoad3FBoulder:
 	jumpstd StrengthBoulderScript
+
+VictoryRoadBoulderFellThroughText:
+	text "The boulder fell"
+	line "through."
+	done
 
 VictoryRoad3F_MapEvents:
 	db 0, 0 ; filler
@@ -22,7 +62,7 @@ VictoryRoad3F_MapEvents:
 	warp_event   2,  0, VICTORY_ROAD_2F, 3
 	warp_event  27, 15, VICTORY_ROAD_2F, 4
 	warp_event  26,  8, VICTORY_ROAD_2F, 5
-	warp_event  23, 15, VICTORY_ROAD_2F, 8
+	warp_event  23, 15, VICTORY_ROAD_2F, 8 ; pit
 	;warp_event 13, 17, VICTORY_ROAD, 4
 	;warp_event 17, 33, VICTORY_ROAD, 7
 	;warp_event 17, 19, VICTORY_ROAD, 6
@@ -39,8 +79,8 @@ VictoryRoad3F_MapEvents:
 	;bg_event  3, 65, BGEVENT_ITEM, VictoryRoadHiddenFullHeal
 
 	def_object_events
+	object_event  22, 15, SPRITE_BOULDER_KANTO, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, VictoryRoad3FBoulder, EVENT_BOULDER_IN_VICTORY_ROAD_1
 	object_event  13, 12, SPRITE_BOULDER_KANTO, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, VictoryRoad3FBoulder, -1
-	object_event  22, 15, SPRITE_BOULDER_KANTO, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, VictoryRoad3FBoulder, -1
 	object_event  24, 10, SPRITE_BOULDER_KANTO, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, VictoryRoad3FBoulder, -1
 	object_event  22,  3, SPRITE_BOULDER_KANTO, SPRITEMOVEDATA_STRENGTH_BOULDER, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, VictoryRoad3FBoulder, -1
 	;object_event 18, 13, SPRITE_RIVAL, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_RIVAL_VICTORY_ROAD
